ec40cd1c0f0fcc396fd9c26d9ecf3c0297242e18
mayrant <mayrant2004@yahoo.com>
2021-06-18 00:48:59 -0500

    added functioning js html and css code to create list of random users

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # JS211_AddressBook
-Fetch results from an API and render them to the DOM
+List of Contacts from a Random User API
 
-- [] Use the API: https://randomuser.me/api/ to fetch a user now
-- [] Fetch a new user multiple times and store them in an array
-- [] Then list out all the users in your address book array by name and picture
-- [] Figure out how to fetch multiple users in one fetch request
-- [] Fetch multiple users on window load
-- [] Add a button to each user that when clicked displays the rest of their information like DOB, address and so forth
-- [] Once you have the functionality working, feel free to style and structure your address book with CSS and HTML
+- Use the API: https://randomuser.me/api/ to fetch a user now
+- Fetch a new user multiple times and store them in an array
+- Then list out all the users in your address book array by name and picture
+- Figure out how to fetch multiple users in one fetch request
+- Fetch multiple users on window load
+- Add a button to each user that when clicked displays the rest of their information like DOB, address and so forth
+- Once you have the functionality working, feel free to style and structure your address book with CSS and HTML
---
+++ main.js
@@ -0,0 +1,60 @@
+// basic fetch for reference
+fetch('https://randomuser.me/api/')
+  .then(res => res.json())
+  .then(data => console.log('Basic Fetch Results:', data.results[0]));
+
+// target the ul element from the HTML file
+const list = document.getElementById('list');
+
+// create a variable for the api url
+const API = 'https://randomuser.me/api/?results=15';
+
+// function for fetching data and rendering it to the DOM
+const fetchUsers = () => {
+  fetch(API)
+  .then(res => res.json())
+  .then((data) => {
+    data.results.forEach((person) => {
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+  
+      img.src = person.picture.thumbnail;
+      img.alt = person.name.first;
+  
+      li.appendChild(img);
+      
+      const h4 = document.createElement('h4');
+      const button = document.createElement('button');
+  
+      h4.innerText = `${person.name.first} ${person.name.last}`;
+      button.innerText = 'More Info';
+  
+      li.appendChild(h4);
+      li.appendChild(button);
+      
+      // the display info function runs when a button is clicked
+      button.onclick = function displayInfo() {
+        // create a paragraph element to have a place for the info to be displayed 
+        const p = document.createElement('p');
+        
+        // use template literal to create a list of the person's info
+        let info = `
+        Name: ${person.name.first} ${person.name.last}
+        Age: ${person.dob.age}
+        City, State: ${person.location.city}, ${person.location.state}
+        Country: ${person.location.country}
+        Email: ${person.email}
+        `;
+        
+        // set the inner text of the paragragh to be the person's info 
+        p.innerText = info;
+        // add the paragrah to the list item
+        li.appendChild(p);
+      }
+      list.appendChild(li);
+    })
+  })
+}
+
+// run the fetch users function when the window loads
+window.onload = fetchUsers();
